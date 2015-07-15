c4fa21d415268db5af2a2c29263e14dd3fadf1c6
Anjanette <younga3@uw.edu>
2015-07-15 13:10:06 -0700

    Add epel repo before trying to install redis and nodejs.

--- Vagrantfile.nix
+++ Vagrantfile.nix
@@ -10,8 +10,8 @@ Vagrant.configure(2) do |config|
 
   config.vm.network "forwarded_port", guest:80, host: 8080
 
-  config.ssh.private_key_path = "~/.ssh/id_rsa"
-  config.ssh.forward_agent = true
+  #config.ssh.private_key_path = "~/.ssh/id_rsa"
+  #config.ssh.forward_agent = true
 
   config.vm.provision :ansible do |ansible|
     ansible.playbook = "playbook.yml"
