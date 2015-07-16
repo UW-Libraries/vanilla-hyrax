041ee91149b05ebbe9345da2f4190ba7e3bc2db1
Anjanette <younga3@uw.edu>
2015-07-16 14:08:31 -0700

    Update templates to reflect jetty instead of tomcat in dev and test.

--- install-win.sh
+++ install-win.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#sudo yum -y update
+sudo yum -y update
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum -y install ansible
 
