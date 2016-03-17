c0410597dd4e791fe07058d3b3f936b3e168f6ec
Anjanette <younga3@uw.edu>
2016-03-17 14:29:31 -0700

    Build sufia.

--- install-win.sh
+++ install-win.sh
@@ -4,7 +4,7 @@ sudo yum -y update
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum -y install ansible
 #ansible-galaxy install -p ~/sync/roles rvm_io.rmv1-ruby
-cd ~/sync
+#cd /home/vagrant/sync
 #ansible-playbook -i inventory playbook.yml
 #mv /vagrant/inventory ~/vagrant/inventory
 
