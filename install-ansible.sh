2770a158b225074ede3af206081fb9a7dfaba85d
Anjanette <younga3@uw.edu>
2016-08-02 16:17:16 -0700

    Fix fits again

--- install-ansible.sh
+++ install-ansible.sh
@@ -3,6 +3,8 @@
 sudo yum -y update
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum -y install ansible
+#sudo yum -y install libffi-devel libssl-devel python-devel python-pip
+#sudo pip install ansible
 
 #ansible-galaxy install -p ~/sync/roles rvm_io.rmv1-ruby
 
