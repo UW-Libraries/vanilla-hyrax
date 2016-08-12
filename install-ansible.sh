f0cdbc94644a2662f4016fcf4b6bde23b4415a63
Anjanette <younga3@uw.edu>
2016-08-12 15:51:45 -0700

    Clean up and make more generic this sufia install.

--- install-ansible.sh
+++ install-ansible.sh
@@ -3,10 +3,3 @@
 sudo yum -y update
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum -y install ansible
-#sudo yum -y install libffi-devel libssl-devel python-devel python-pip
-#sudo pip install ansible
-
-#ansible-galaxy install -p ~/sync/roles rvm_io.rmv1-ruby
-
-#cd /home/vagrant/sync
-#ansible-playbook -i inventory playbook.yml
