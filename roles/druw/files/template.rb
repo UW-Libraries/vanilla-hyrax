57b3c34d9c73b5efefb90471fba9b441b65d60b6
Anjanette <younga3@uw.edu>
2016-11-07 13:28:06 -0800

    Add druw and druw_admin roles.

---
+++ roles/druw/files/template.rb
@@ -0,0 +1,14 @@
+#gem 'sufia', '7.2.0'
+gem 'sufia', 'master'
+gem 'flipflop', github: 'jcoyne/flipflop', branch: 'hydra'
+
+run 'bundle install'
+
+generate 'sufia:install', '-f'
+
+# Support Rails 4.2 and 5.0
+begin
+  rails_command 'db:migrate'
+rescue NoMethodError
+  rake 'db:migrate'
+end
